b336fdf695ccffd736ed4b5cdaea7c654f9c258f
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 00:11:39 +0000

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c "United Kingdom" -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+# sudo virsh net-autostart default
+
+
+sudo pacman -S gdm gnome firefox gnome-tweaks arc-gtk-theme arc-icon-theme vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+# sudo flatpak install -y spotify
+# sudo flatpak install -y kdenlive
+
+sudo systemctl enable gdm
