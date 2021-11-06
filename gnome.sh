ba8e8a4f1cf01f6931dcb51c7649a4e41a9dc8ca
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-06 22:38:45 +0000

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -163,6 +163,7 @@ gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable drive-menu@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable apps-menu@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable ding@rastersoft.com
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/archlinux/simple.png
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.Terminal.desktop', 'gnome-control-center.desktop']"
