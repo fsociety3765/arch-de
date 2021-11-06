2e8cf9b0508b889b65f2feb72a6aa295c5007004
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-06 22:26:09 +0000

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -166,7 +166,7 @@ sudo sed -i "s/#EnableFlatpak/EnableFlatpak/g" /etc/pamac.conf
 sudo sed -i "s/#CheckFlatpakUpdates/CheckFlatpakUpdates/g" /etc/pamac.conf
 sudo sed -i "s/#EnableSnap/EnableSnap/g" /etc/pamac.conf
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
-sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo './assets/background.jpg'
+sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo './background.jpg'
 gsettings set org.gnome.desktop.app-folders folder-children "['Office', 'Accessories', 'System']"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/System/ name "System Tools"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Office/ name "Office"
