2df661ada1ca6b1b4161840f503b40f9d6eb95f9
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-07 03:44:36 +0000

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -141,6 +141,15 @@ done
 #  paru -Rcns "$PKG" --noconfirm
 #done
 
+if lspci | grep -E "NVIDIA|GeForce"; then
+    pacman -S nvidia --noconfirm --needed
+	nvidia-xconfig
+elif lspci | grep -E "Radeon"; then
+    pacman -S xf86-video-amdgpu --noconfirm --needed
+elif lspci | grep -E "Integrated Graphics Controller"; then
+    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+fi
+
 echo "-------------------------------------------------"
 echo "Enabling services to run at boot  "
 echo "-------------------------------------------------"
